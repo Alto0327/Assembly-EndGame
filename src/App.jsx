0918f3a81ce59700737230b7d86ce343b75bb645
Alto0327 <aj.silvaf2003@gmail.com>
2025-05-13 11:07:05 -0400

    Added Word Display

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import Header from "./Components/Header";
 import './App.css'
 import Languages from "./Components/Languages";
@@ -6,9 +6,22 @@ import { languages } from "./languages";
 
 
 export default function App(){
-    const languagesElements = languages.map((language)=>{
-        return <Languages key={language.name} name={language.name} backgroundColor={language.backgroundColor} color={language.color}/>
-    })
+    const [currentWord, setCurrentWord] = useState('react')
+    
+    const languagesElements = languages.map(language=>(
+            <Languages 
+                key={language.name} 
+                name={language.name} 
+                backgroundColor={language.backgroundColor} 
+                color={language.color}
+            />
+    ))
+
+    const letterElements = currentWord.split("").map((letter,i) => (
+        <span key={i}>{letter.toUpperCase()}</span>
+    ))
+
+    console.log(letterElements)
     return(
         <>
             <Header/>
@@ -18,6 +31,9 @@ export default function App(){
             </section>
             <section className="languages-container">
                 {languagesElements}
+            </section>
+            <section className="word">
+                {letterElements}
             </section>
         </>
     )
